d7f7d7a22e6d3ec3272e79d3fa682578db1ff2e0
Gökmen Görgen <gkmngrgn@gmail.com>
2018-05-06 14:15:58 +0200

    fix model - admin config.

--- Article-05/hello_django/hello_uptime/admin.py
+++ Article-05/hello_django/hello_uptime/admin.py
@@ -5,7 +5,7 @@ from hello_uptime.models import Monitor
 
 @admin.register(Monitor)
 class MonitorAdmin(admin.ModelAdmin):
-    list_display = ('url', 'status', 'interval', 'checked_at', 'created_at', 'is_active')
+    list_display = ('url', 'interval', 'status', 'checked_at', 'created_at', 'is_active')
     list_filter = ('is_active', 'status', 'interval', 'checked_at')
     date_hierarchy = 'created_at'
     search_fields = ('url',)
--- Article-05/hello_django/hello_uptime/models.py
+++ Article-05/hello_django/hello_uptime/models.py
@@ -10,7 +10,7 @@ class Monitor(models.Model):
         default=MonitoringInterval.get_default())
     status = models.CharField(verbose_name=_("Status"), max_length=9, choices=MonitorStatus.get_choices(), blank=True)
     is_active = models.BooleanField(verbose_name=_("Is active?"), blank=True, default=True)
-    checked_at = models.DateTimeField(verbose_name=_("Checked at"), editable=False)
+    checked_at = models.DateTimeField(verbose_name=_("Checked at"), null=True, editable=False)
     created_at = models.DateTimeField(verbose_name=_("Created at"), auto_now_add=True)
 
     class Meta:
