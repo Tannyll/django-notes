1c80ab083ec3cf496ac61f2a5b4b01aa3437cabc
Gökmen Görgen <gkmngrgn@gmail.com>
2018-06-03 15:44:18 +0200

    Article 08: command simplication and frontend improvements.

--- Article-08/hello_django/hello_uptime/management/commands/checkurls.py
+++ Article-08/hello_django/hello_uptime/management/commands/checkurls.py
@@ -1,8 +1,6 @@
 import requests
-
-from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
-from django.core.validators import URLValidator
+from django.utils import timezone
 from requests.exceptions import SSLError
 
 from hello_uptime.models import Monitor
@@ -16,23 +14,11 @@ class Command(BaseCommand):
         parser.add_argument('urls', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        valid_urls, invalid_urls = self.get_urls(options)
+        urls = options['urls']
 
-        if not valid_urls:
-            self.stderr.write(self.style.ERROR("There's no valid urls, please check again."))
-            return
-
-        if invalid_urls:
-            self.stderr.write(self.style.ERROR("We passed these invalid urls:"))
-            for invalid_url in invalid_urls:
-                self.stderr.write(self.style.ERROR("\t- {}".format(invalid_url)))
-
-        for url in valid_urls:
+        for url in urls:
             monitors = Monitor.objects.filter(is_active=True, url=url)
             self.stdout.write(self.style.WARNING("{} - {} monitor(s)".format(url, monitors.count())), ending=': ')
-            if not monitors.exists():
-                self.stdout.write(self.style.WARNING("passed"))
-                continue
 
             try:
                 response = requests.get(url)
@@ -40,22 +26,6 @@ class Command(BaseCommand):
             except SSLError:
                 status = MonitorStatus.OFFLINE
 
-            monitors.update(status=status)
+            monitors.update(status=status, checked_at=timezone.now())
             status_style = self.style.ERROR if status == MonitorStatus.OFFLINE else self.style.SUCCESS
             self.stdout.write(status_style(status))
-
-    def get_urls(self, options):
-        urls = options['urls']
-        valid_urls = []
-        invalid_urls = []
-        validate_url = URLValidator(schemes=['http', 'https'])
-
-        for url in urls:
-            try:
-                validate_url(url)
-            except ValidationError:
-                invalid_urls.append(url)
-            else:
-                valid_urls.append(url)
-
-        return valid_urls, invalid_urls
