69e03c39b4ca9a6f554e6cbd883c9df87080400b
Gökmen Görgen <gkmngrgn@gmail.com>
2018-06-11 00:09:23 +0200

    new celery task for checking monitors.

--- Article-09/hello_django/hello_uptime/tasks.py
+++ Article-09/hello_django/hello_uptime/tasks.py
@@ -1,6 +1,12 @@
 from celery import shared_task
+from django.core.management import call_command
 
 
 @shared_task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@shared_task
+def check_monitors():
+    call_command('checkurls', mail_clients=True)
